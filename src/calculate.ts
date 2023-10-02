4522646ab318efdbf174ca08b1d21c78de535aaa
Alex Forshtat <forshtat1@gmail.com>
2023-10-02 22:02:58 +0300

    Rewrite calculation to collect all addresses refund in one iteration

--- src/calculate.ts
+++ src/calculate.ts
@@ -17,37 +17,40 @@ interface AddressAccessDetails {
   slots: Slot[]
 }
 
+interface Refund {
+  refundFromBurn: bigint
+  refundFromCoinbase: bigint
+}
+
 export function calculateColdStorageRefund (
-  sender: string,
   baseFeePerGas: string,
-  accessDetailsMap: AddressAccessDetails[]
-) {
-  let refundFromBurn = 0
-  let refundFromCoinbase = 0
+  accessDetailsMap: AddressAccessDetails[]): Map<string, Refund> {
+
+  const refunds = new Map<string, Refund>()
+
   for (const accessDetail of accessDetailsMap) {
-    const coldAccountAccess = accessDetail.accessors.find(it => it.address === sender)
-    if (coldAccountAccess != null) {
-      const addressAccessN = accessDetail.accessors.length
-      const refundPercent = (addressAccessN - 1) / addressAccessN
-      for (const priorityFeePerGas of coldAccountAccess.priorityFeePerGas) {
-        refundFromBurn += COLD_ACCOUNT_ACCESS_COST * parseInt(baseFeePerGas) * refundPercent
-        refundFromCoinbase += COLD_ACCOUNT_ACCESS_COST * parseInt(priorityFeePerGas) * refundPercent
+    const addressAccessN = accessDetail.accessors.length
+    const refundPercent = (addressAccessN - 1) / addressAccessN
+    for (const accessor of accessDetail.accessors) {
+      for (const priorityFeePerGas of accessor.priorityFeePerGas) {
+        const refund = refunds.get(accessor.address) ?? { refundFromBurn: 0n, refundFromCoinbase: 0n }
+        refund.refundFromBurn += BigInt(Math.floor(COLD_ACCOUNT_ACCESS_COST * parseInt(baseFeePerGas) * refundPercent))
+        refund.refundFromCoinbase += BigInt(Math.floor(COLD_ACCOUNT_ACCESS_COST * parseInt(priorityFeePerGas) * refundPercent))
+        refunds.set(accessor.address, refund)
       }
-      for (const slot of accessDetail.slots) {
-        const coldSlotAccess = slot.accessors.find(it => it.address === sender)
-        if (coldSlotAccess != null) {
-          const slotAccessN = slot.accessors.length
-          const refundPercent = (slotAccessN - 1) / slotAccessN
-          for (const priorityFeePerGas of coldSlotAccess.priorityFeePerGas) {
-            refundFromBurn += COLD_SLOAD_COST * parseInt(baseFeePerGas) * refundPercent
-            refundFromCoinbase += COLD_SLOAD_COST * parseInt(priorityFeePerGas) * refundPercent
-          }
+    }
+    for (const slot of accessDetail.slots) {
+      for (const accessor of slot.accessors) {
+        const slotAccessN = slot.accessors.length
+        const refundPercent = (slotAccessN - 1) / slotAccessN
+        for (const priorityFeePerGas of accessor.priorityFeePerGas) {
+          const refund = refunds.get(accessor.address) ?? { refundFromBurn: 0n, refundFromCoinbase: 0n }
+          refund.refundFromBurn += BigInt(Math.floor(COLD_SLOAD_COST * parseInt(baseFeePerGas) * refundPercent))
+          refund.refundFromCoinbase += BigInt(Math.floor(COLD_SLOAD_COST * parseInt(priorityFeePerGas) * refundPercent))
+          refunds.set(accessor.address, refund)
         }
       }
     }
   }
-  return {
-    refundFromBurn: Math.floor(refundFromBurn),
-    refundFromCoinbase: Math.floor(refundFromCoinbase)
-  }
+  return refunds
 }
--- test/calculate.test.ts
+++ test/calculate.test.ts
@@ -5,10 +5,22 @@ import { calculateColdStorageRefund } from '../src/calculate'
 
 describe('calculateColdStorageRefund', () => {
   it('test', function () {
-      const refund = calculateColdStorageRefund(
-        '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1', '10000', blockAddressMap
+      const refund = calculateColdStorageRefund('10000', blockAddressMap
       )
-      assert.equal(refund, {})
+      assert.deepEqual(Object.fromEntries(refund), {
+        '0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b': {
+          refundFromBurn: 55666666n,
+          refundFromCoinbase: 13916666n
+        },
+        '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1': {
+          refundFromBurn: 55666666n,
+          refundFromCoinbase: 8349999n
+        },
+        '0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0': {
+          refundFromBurn: 34666666n,
+          refundFromCoinbase: 5199999n
+        }
+      })
     }
   )
 })
