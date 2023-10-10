093639e561cef09e27e5270e18b5998421b729e2
Alex Forshtat <forshtat1@gmail.com>
2023-10-11 02:24:37 +0300

    Rewrite to make the code easier to read

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,108 +1,108 @@
-const COLD_ACCOUNT_ACCESS_COST = 2600
-const COLD_SLOAD_COST = 2100
+const COLD_ACCOUNT_ACCESS_COST = '2600'
+const COLD_SLOAD_COST = '2100'
 
-interface AccessDetails {
+export interface AccessDetails {
   sender: string
   priorityFeePerGas: string
 }
 
-interface Slot {
+export interface Slot {
   id: string
   accessors: AccessDetails[]
 }
 
-interface AddressAccessDetails {
+export interface AddressAccessDetails {
   address: string
   accessors: AccessDetails[]
   slots: Slot[]
 }
 
-interface Refund {
+export interface Refund {
   refundFromBurn: bigint
   refundFromCoinbase: bigint
 }
 
-export function calculateColdStorageRefund (
+/**
+ * The main function used to calculate the entire redistribution of the cold access costs in a block.
+ * @param baseFeePerGas - the base fee per gas parameter of the block.
+ * @param accessDetailsMap - the details of each cold access of an address or a slot including transaction priority fee
+ */
+export function calculateBlockColdAccessRefund (
   baseFeePerGas: string,
-  accessDetailsMap: AddressAccessDetails[]): Map<string, Refund> {
-
+  accessDetailsMap: AddressAccessDetails[]
+): Map<string, Refund> {
   const refunds = new Map<string, Refund>()
-
   for (const accessDetail of accessDetailsMap) {
-    const addressAccessN = accessDetail.accessors.length
-    const refundPercent = (addressAccessN - 1) / addressAccessN
-    for (const accessor of accessDetail.accessors) {
-      for (const priorityFeePerGas of accessor.priorityFeePerGas) {
-        const refund = refunds.get(accessor.sender) ?? { refundFromBurn: 0n, refundFromCoinbase: 0n }
-        refund.refundFromBurn += BigInt(Math.floor(COLD_ACCOUNT_ACCESS_COST * parseInt(baseFeePerGas) * refundPercent))
-        // refund.refundFromCoinbase += BigInt(Math.floor(COLD_ACCOUNT_ACCESS_COST * parseInt(priorityFeePerGas) * refundPercent))
-        refunds.set(accessor.sender, refund)
-      }
-    }
+    calculateItemColdAccessRefund(accessDetail.accessors, baseFeePerGas, COLD_ACCOUNT_ACCESS_COST, refunds)
     for (const slot of accessDetail.slots) {
-      for (const accessor of slot.accessors) {
-        const slotAccessN = slot.accessors.length
-        const refundPercent = (slotAccessN - 1) / slotAccessN
-        for (const priorityFeePerGas of accessor.priorityFeePerGas) {
-          const refund = refunds.get(accessor.sender) ?? { refundFromBurn: 0n, refundFromCoinbase: 0n }
-          refund.refundFromBurn += BigInt(Math.floor(COLD_SLOAD_COST * parseInt(baseFeePerGas) * refundPercent))
-          // refund.refundFromCoinbase += BigInt(Math.floor(COLD_SLOAD_COST * parseInt(priorityFeePerGas) * refundPercent))
-          refunds.set(accessor.sender, refund)
-        }
-      }
+      calculateItemColdAccessRefund(slot.accessors, baseFeePerGas, COLD_SLOAD_COST, refunds)
     }
   }
   return refunds
 }
 
-export function calculatePriorityFeeRefund (accessDetails: AccessDetails[], accessGasCost: number) {
-  // 1. Sort all accesses by their "priorityFeePerGas"
-  const sortedAccesses = accessDetails.sort((a, b) => {
-    return parseInt(b.priorityFeePerGas) - parseInt(a.priorityFeePerGas)
-  })
+/**
+ * Inner function to calculate a refund for a single accessed element.
+ * Does not return - updates the {@link refunds} map.
+ * @param unsortedAccessors - the array with information of access event without sorting.
+ * @param baseFeePerGas - the base fee per gas parameter of the block.
+ * @param accessGasCost - the gas cost of the access operation.
+ * @param refunds - the mapping to store the results.
+ */
+function calculateItemColdAccessRefund (
+  unsortedAccessors: AccessDetails[],
+  baseFeePerGas: string,
+  accessGasCost: string,
+  refunds: Map<string, Refund>
+): void {
+  const sortedAccessDetails = unsortedAccessors.sort((a, b) => { return parseInt(b.priorityFeePerGas) - parseInt(a.priorityFeePerGas) })
+  const addressAccessN = sortedAccessDetails.length
+  const refundPercent = (addressAccessN - 1) / addressAccessN
+  const refundsFromCoinbase = calculatePriorityFeeRefunds(sortedAccessDetails, accessGasCost)
+  for (let i = 0; i < sortedAccessDetails.length; i++) {
+    const accessor = sortedAccessDetails[i]
+    const refund = refunds.get(accessor.sender) ?? { refundFromBurn: 0n, refundFromCoinbase: 0n }
+    refund.refundFromBurn += BigInt(Math.floor(parseInt(accessGasCost) * parseInt(baseFeePerGas) * refundPercent))
+    refund.refundFromCoinbase += BigInt(refundsFromCoinbase[i])
+    refunds.set(accessor.sender, refund)
+  }
+}
 
-  // 2. Calculate "contribution" as an increase in total refund caused by including this transaction
+/**
+ * Calculate a reasonably fair distribution of priority fee refunds based on each transaction's contribution
+ * to the total refund amount.
+ * @param sortedAccesses - an array of {@link AccessDetails} already sorted by the {@link priorityFeePerGas}.
+ * @param accessGasCost - the amount of gas units consumed by a single access operation
+ */
+export function calculatePriorityFeeRefunds (sortedAccesses: AccessDetails[], accessGasCost: string) {
+  // Validator charge is based on the highest paid priority fee per gas
+  const validatorFee = parseInt(sortedAccesses[0].priorityFeePerGas) * parseInt(accessGasCost)
   // Notice that the two most expensive transactions have the same contribution to the refund
-  // All the rest of transactions contribute all of their cost to the refund
-  const topTransactionContribution = parseInt(sortedAccesses[1].priorityFeePerGas) * accessGasCost
-  const refundIncrease = sortedAccesses.map((value, index) => {
-    const charge = parseInt(value.priorityFeePerGas) * accessGasCost
-    return {
-      sender: value.sender,
-      contribution: index == 0 ? topTransactionContribution : charge,
-      charge
-    }
-  })
+  const topTransactionContribution = parseInt(sortedAccesses[1].priorityFeePerGas) * parseInt(accessGasCost)
 
-  // 3. Calculate the sum of all "contributions"
-  const totalContributions = refundIncrease.reduce((previousValue, currentValue) => {
-    return previousValue + currentValue.contribution
-  }, 0)
+  let totalContributions = 0  // Accumulate the sum of all "contributions"
+  let totalSendersCharged = 0 // Accumulate cost of gas paid to validator for accessing the same address/slot/chunk
+  for (let i = 0; i < sortedAccesses.length; i++) {
+    const charge = parseInt(sortedAccesses[i].priorityFeePerGas) * parseInt(accessGasCost)
+    const contribution = i == 0 ? topTransactionContribution : charge
+    totalContributions += contribution
+    totalSendersCharged += charge
+  }
 
-  // 4. Calculate the remainder of gas paid to validator for accessing the same address/slot/chunk
-  const totalSendersCharged = sortedAccesses.reduce(
-    (previousValue, currentValue) => {
-      return previousValue + parseInt(currentValue.priorityFeePerGas) * accessGasCost
-    }, 0)
-
-  // 5. Validator charge is "the highest price * gas cost"
-  const validatorFee = parseInt(sortedAccesses[0].priorityFeePerGas) * accessGasCost
-
-  // 6. Calculate the total amount of ether to be refunded for this access
+  // Calculate the total amount of ether to be refunded for this access
   const totalRefund = totalSendersCharged - validatorFee
+  if (totalRefund == 0) {
+    // protect from NaN if all priority fees are 0
+    return Array(sortedAccesses.length).fill(0)
+  }
 
-  // 7. Calculate actual charges and refunds
+  // Calculate actual charges and refunds
+  const refunds = []
   const ratio = totalRefund / totalContributions
-  const refunds = refundIncrease.map((it) => {
-    const refund = Math.floor(ratio * it.contribution)
-    const actualCharge = it.charge - refund
-    return {
-      sender: it.sender,
-      originalCharge: it.charge,
-      actualCharge,
-      refund
-    }
-  })
-  console.log(refunds)
+  for (let i = 0; i < sortedAccesses.length; i++) {
+    const charge = parseInt(sortedAccesses[i].priorityFeePerGas) * parseInt(accessGasCost)
+    const contribution = i == 0 ? topTransactionContribution : charge
+    refunds.push(Math.floor(ratio * contribution))
+  }
   return refunds
 }
--- test/calculate.test.ts
+++ test/calculate.test.ts
@@ -1,24 +1,24 @@
 import blockAddressMap from '../assets/blockAccessMap2.json'
 import assert from 'node:assert'
 
-import { calculateColdStorageRefund, calculatePriorityFeeRefund } from '../src/calculate'
+import { AccessDetails, calculateBlockColdAccessRefund, calculatePriorityFeeRefunds } from '../src/calculate'
 
 describe('calculateColdStorageRefund', function () {
   it('test', function () {
-      const refund = calculateColdStorageRefund('10000', blockAddressMap
+      const refund = calculateBlockColdAccessRefund('10000', blockAddressMap
       )
       assert.deepEqual(Object.fromEntries(refund), {
         '0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b': {
-          refundFromBurn: 55666666n,
-          refundFromCoinbase: 13916666n
+          refundFromBurn: 74833332n,
+          refundFromCoinbase: 14320000n
         },
         '0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1': {
-          refundFromBurn: 55666666n,
-          refundFromCoinbase: 8349999n
+          refundFromBurn: 74833332n,
+          refundFromCoinbase: 10740000n
         },
         '0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0': {
-          refundFromBurn: 34666666n,
-          refundFromCoinbase: 5199999n
+          refundFromBurn: 64333332n,
+          refundFromCoinbase: 8340000n
         }
       })
     }
@@ -29,15 +29,12 @@ describe('calculatePriorityFeeRefund', function () {
 
   const inputs = [
     [
-      { sender: 'A', priorityFeePerGas: '1' },
-    ],
-    [
-      { sender: 'A', priorityFeePerGas: '1' },
-      { sender: 'B', priorityFeePerGas: '1' },
+      { sender: 'A', priorityFeePerGas: '100' },
+      { sender: 'B', priorityFeePerGas: '10' },
     ],
     [
       { sender: 'A', priorityFeePerGas: '100' },
-      { sender: 'B', priorityFeePerGas: '10' },
+      { sender: 'B', priorityFeePerGas: '100' },
     ],
     [
       { sender: 'A', priorityFeePerGas: '100' },
@@ -54,53 +51,50 @@ describe('calculatePriorityFeeRefund', function () {
       { sender: 'M2', priorityFeePerGas: '1000000' }
     ],
     [
+      { sender: 'M1', priorityFeePerGas: '1000000' },
       { sender: 'A', priorityFeePerGas: '100' },
       { sender: 'B', priorityFeePerGas: '10' },
       { sender: 'C', priorityFeePerGas: '1' },
+    ],
+    [
       { sender: 'M1', priorityFeePerGas: '1000000' },
       { sender: 'M2', priorityFeePerGas: '1000000' },
+      { sender: 'A', priorityFeePerGas: '100' },
+      { sender: 'B', priorityFeePerGas: '10' },
+      { sender: 'C', priorityFeePerGas: '1' },
+    ],
+    [
+      { sender: 'A', priorityFeePerGas: '18' },
+      { sender: 'B', priorityFeePerGas: '11' },
+      { sender: 'C', priorityFeePerGas: '10' },
+      { sender: 'D', priorityFeePerGas: '10' },
+      { sender: 'E', priorityFeePerGas: '9' },
+      { sender: 'F', priorityFeePerGas: '3' },
     ]
   ]
 
   const expected = [
-    [
-      { sender: 'A', eventualCharge: 1000, refund: 0 }
-    ],
-    [
-      { sender: 'A', eventualCharge: 500, refund: 500 },
-      { sender: 'B', eventualCharge: 500, refund: 500 }
-    ],
-    [
-      { sender: 'A', eventualCharge: 90910, refund: 9090 },
-      { sender: 'B', eventualCharge: 9091, refund: 909 }
-    ],
-    [
-      { sender: 'A', eventualCharge: 90910, refund: 9090 },
-      { sender: 'B', eventualCharge: 9091, refund: 909 },
-      { sender: 'C', eventualCharge: 0, refund: 0 }
-    ],
-    [
-      { sender: 'A', eventualCharge: 90091, refund: 9909 },
-      { sender: 'B', eventualCharge: 9010, refund: 990 },
-      { sender: 'C', eventualCharge: 901, refund: 99 }
-    ],
-    [
-      { sender: 'M1', eventualCharge: 500000000, refund: 500000000 },
-      { sender: 'M2', eventualCharge: 500000000, refund: 500000000 },
-    ],
-    [
-      { sender: 'M1', eventualCharge: 499972252, refund: 500027748 },
-      { sender: 'M2', eventualCharge: 499972252, refund: 500027748 },
-      { sender: 'A', eventualCharge: 49998, refund: 50002 },
-      { sender: 'B', eventualCharge: 5000, refund: 5000 },
-      { sender: 'C', eventualCharge: 500, refund: 500 }
-    ]
+    [5000, 5000],           // {tx1: 100, tx2: 10} => refund of 10 split 1/2
+    [50000, 50000],         // {tx1: 100, tx2: 100} => refund of 100 split 1/2
+    [5000, 5000, 0],        // {tx1: 100, tx2: 10, tx3: 0} => refund of 10 split 1/2 and tx3 gets zero
+    [5238, 5238, 523],      // {tx1: 100, tx2: 10, tx3: 1} => refund of 11 split mostly between top two transactions
+    [500000000, 500000000], // {tx1: 1M, tx2: 1M} => equal split
+    [52606,                 // {tx1: 1M, tx1: 100, tx2: 10, tx3: 1} => refund 100 split mostly between the top two txs
+      52606, 5260, 526],
+    [500027748, 500027748,  // {tx1: 1M, tx1: 1M, tx3: 100, tx4: 10, tx5: 1} => refund becomes 1M
+      50002, 5000, 500],
+    [8759, 8759, 7962,      // realistic block with similar priority fees get similar refunds
+      7962, 7166, 2388]
   ]
 
+  function testName (detail: AccessDetails[]): string {
+    return detail.reduce((prev, curr) => {return `${prev}${curr.sender}:${curr.priorityFeePerGas};`}, '')
+  }
+
   for (let i = 0; i < inputs.length; i++) {
-    it.only('should work', function () {
-      const refunds = calculatePriorityFeeRefund(
-        inputs[i], 1000
+    it(`should calculate priority fee refund (${testName(inputs[i])})`, function () {
+      const refunds = calculatePriorityFeeRefunds(
+        inputs[i], '1000'
       )
       assert.deepEqual(refunds, expected[i])
     })
